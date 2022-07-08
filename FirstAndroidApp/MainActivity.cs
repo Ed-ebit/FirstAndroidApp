d79ea1a2a18162e0c7ae968ed16b26b830232240
KMV <karl-martin.vogel@tc.de>
2022-07-08 16:01:14 +0200

    first

--- FirstAndroidApp/MainActivity.cs
+++ FirstAndroidApp/MainActivity.cs
@@ -11,7 +11,7 @@ using Google.Android.Material.Snackbar;
 
 namespace FirstAndroidApp
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(/*Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar",*/ MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
         TextView txtNumber;
@@ -20,44 +20,44 @@ namespace FirstAndroidApp
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.content_main);
 
             //Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             //SetSupportActionBar(toolbar);
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            //FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            //fab.Click += FabOnClick;
 
-            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
-            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
-            txtNumber.Text = (++number).ToString();
-            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
-            txtNumber.Text = (--number).ToString();
+            //txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+            //FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+            //txtNumber.Text = (++number).ToString();
+            //FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+            //txtNumber.Text = (--number).ToString();
         }
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
+        //public override bool OnCreateOptionsMenu(IMenu menu)
+        //{
+        //    MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+        //    return true;
+        //}
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
-            {
-                return true;
-            }
+        //public override bool OnOptionsItemSelected(IMenuItem item)
+        //{
+        //    int id = item.ItemId;
+        //    if (id == Resource.Id.action_settings)
+        //    {
+        //        return true;
+        //    }
 
-            return base.OnOptionsItemSelected(item);
-        }
+        //    return base.OnOptionsItemSelected(item);
+        //}
 
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (View.IOnClickListener)null).Show();
-        }
+        //private void FabOnClick(object sender, EventArgs eventArgs)
+        //{
+        //    View view = (View) sender;
+        //    Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+        //        .SetAction("Action", (View.IOnClickListener)null).Show();
+        //}
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
